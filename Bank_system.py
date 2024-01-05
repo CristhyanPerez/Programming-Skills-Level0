67e175f39a7531bd385a6592bff1506cc82e7425
Cristhyan Perez <pevicristhyan@gmail.com>
2024-01-04 23:38:51 -0500

    Banking system initial menu

---
+++ Bank_system.py
@@ -0,0 +1,25 @@
+menu = """
+Welcome to the online Banking System:
+
+Notes:
+*The password is made up of the following:
+Username + space + number of characteres in the user word
+
+Example:
+Username: Cristhyan
+Password: Cris 9
+
+*You will only have three attempts.
+
+********** Log in **********
+"""
+print(menu)
+
+username = input("What is your username?: ")
+password = input("What is your password?: ")
+
+
+
+print(f"Hola {username} and {password}")
+
+
