50fba6d627e9d3e5a1bba9a0d5676e375cf77621
Cristhyan Perez <pevicristhyan@gmail.com>
2024-01-05 14:59:47 -0500

    ex01 - First Version

--- Bank_system.py
+++ Bank_system.py
@@ -43,10 +43,13 @@ menu_02 = """
 5. Exit
 """
 answer = "y"
+add_money = 0
 
-def options(number_operation):
+def options(number_operation, balance):
     if number_operation == "1":
-        print("\nElegiste 1")
+        print("\nHow much are you going to deposit?: ")
+        add_money = input("$ " )
+        add_money = int(add_money)
     elif number_operation == "2":
         print("\nElegiste 2")
     elif number_operation == "3":
@@ -54,6 +57,7 @@ def options(number_operation):
         print(balance)
     elif number_operation == "4":
         print("\nElegiste 4")
+    balance_final = balance + add_money
     
 if access == True:
     while answer == "y" or answer == "yes":
@@ -62,7 +66,7 @@ if access == True:
         print(menu_02)
         n_operation = input("Please, choose a number from 1 to 5 (1-5): ")
         if n_operation == "1" or n_operation == "2" or n_operation == "3" or n_operation == "4":
-            options(n_operation)
+            options(n_operation, balance)
             answer = input("\nDo you want to do another operation? (y/n): ")
             answer = answer.lower()
         elif n_operation == "5":
