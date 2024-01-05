72d1077accee82ca57995d324363e8accd6bf82b
Cristhyan Perez <pevicristhyan@gmail.com>
2024-01-05 01:51:37 -0500

    Update Ex01 - Login 3 attempts

--- Bank_system.py
+++ Bank_system.py
@@ -6,8 +6,8 @@ Notes:
 Username + space + number of characteres in the user word
 
 Example:
-Username: Cristhyan
-Password: Cris 9
+Username: Cris
+Password: Cris 4
 
 *You will only have three attempts.
 
@@ -15,11 +15,34 @@ Password: Cris 9
 """
 print(menu)
 
-username = input("What is your username?: ")
-password = input("What is your password?: ")
+Access = False
+balance = 2000
+
+for i in range (0,3):
+    username = input("What is your username?: ")
+    number_char = len(username)
+    password_system = username + " " +  str(number_char)
+    password_input = input("What is your password?: ")
+    print(f"\nAttempt {i + 1} of 3")
+    
+    if password_input == password_system:
+        print("Correct username and password\n")
+        Access = True
+        break
+    else:
+        print("Warning: Incorrect password...")
+        if i < 2:
+            print("Reset username and pasword\n")
+            print("********** Log in **********\n")
+
+if Access == True:
+    
+    
+else:
+    print("Warning: Maximum number of attempts")
+    print("Locked system")
+    print("Come back soon.\nThank you :)")
 
 
-
-print(f"Hola {username} and {password}")
 
 
