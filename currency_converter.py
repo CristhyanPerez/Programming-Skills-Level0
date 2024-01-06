ed27b97f20deff416d865bfd7077f600f74ce534
Cristhyan Perez <pevicristhyan@gmail.com>
2024-01-06 12:46:41 -0500

    Update Ex02 - Currency converter - convert function

--- currency_converter.py
+++ currency_converter.py
@@ -1,3 +1,20 @@
+exchange_values = [892.86, 811.20, 1.00, 0.91, 29.82, 0.79, 3.69]
+
+def exchange_dolar_other(amount, value_exchange):
+    amount = amount * value_exchange
+    amount = round(amount, 2)
+    return amount
+
+def exchange_other_dolar(amount, value_exchange):
+    amount = amount / value_exchange
+    amount = round(amount, 2)
+    return amount
+
+def exchange_other_other(amount, value_in, value_out):
+    amount_in = exchange_other_dolar(amount, value_in)
+    amount_out = exchange_dolar_other(amount_in, value_out)
+    return amount_out
+
 def symbol_money(number_money):
     if number_money == "1":
         symbol = "CLP"
@@ -11,6 +28,8 @@ def symbol_money(number_money):
         symbol = "TRY"
     elif number_money == "6":
         symbol = "GBP"
+    elif number_money == "7":
+        symbol = "PEN"
     return symbol
 
 initial_message = """
@@ -35,21 +54,37 @@ Currency available to exchange
 4- EUR
 5- TRY
 6- GBP
+7- PEN
 """
 
 print(initial_message)
 print("Choose your initial currency.")
-initial_money = input("From the menu, choose a number form 1 to 5: ")
-if initial_money == "1" or initial_money == "2" or initial_money == "3" or initial_money == "4" or initial_money == "5" or initial_money == "6":
-    currency_symbol = symbol_money(initial_money)
-    balance = input(f"\nHow much funds do you have: {currency_symbol} ")
+initial_money = input("From the menu, choose a number form 1 to 7: ")
+if initial_money == "1" or initial_money == "2" or initial_money == "3" or initial_money == "4" or initial_money == "5" or initial_money == "6" or initial_money == "7":
+    currency_symbol_in = symbol_money(initial_money)
+    balance = input(f"\nHow much funds do you have: {currency_symbol_in} ")
     balance = float(balance)
     balance = round(balance,2)
     if balance >= 0:
         print("\nChoose the currency you want to change to.")
-        final_money = input("From the menu, choose a number form 1 to 5: ")
-        if final_money == "1" or final_money == "2" or final_money == "3" or final_money == "4" or final_money == "5" or initial_money == "6":
-            print("hola")
+        final_money = input("From the menu, choose a number form 1 to 7: ")
+        currency_symbol_out = symbol_money(final_money)
+        if final_money == "1" or final_money == "2" or final_money == "3" or final_money == "4" or final_money == "5" or final_money == "6" or final_money == "7":
+            initial_money = int(initial_money)
+            final_money = int(final_money)
+            initial_money_exchange = exchange_values[initial_money - 1]
+            final_money_exchange = exchange_values[final_money - 1]
+            if initial_money == final_money:
+                print(f"{currency_symbol_in} {balance} is equivalent to {currency_symbol_out} {balance}")
+            elif initial_money == 3 and final_money != 3:
+                exchange_balance = exchange_dolar_other(balance, final_money_exchange)
+                print(f"{currency_symbol_in} {balance} is equivalent to {currency_symbol_out} {exchange_balance}") 
+            elif initial_money != 3 and final_money == 3:
+                exchange_balance = exchange_other_dolar(balance, initial_money_exchange)
+                print(f"{currency_symbol_in} {balance} is equivalent to {currency_symbol_out} {exchange_balance}")
+            else:
+                exchange_balance = exchange_other_other(balance, initial_money_exchange, final_money_exchange)
+                print(f"{currency_symbol_in} {balance} is equivalent to {currency_symbol_out} {exchange_balance}")
         else:
             print("\nSorry. Incorrect number chosen.!!!")
             print("Come back soon. Thanks")
@@ -59,4 +94,3 @@ if initial_money == "1" or initial_money == "2" or initial_money == "3" or initi
 else:
     print("\nSorry. Incorrect number chosen.!!!")
     print("Come back soon. Thanks")
-
