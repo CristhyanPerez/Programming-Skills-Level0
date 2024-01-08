a2eee0c6d99a119af9a627975568974a9c49e985
Cristhyan Perez <pevicristhyan@gmail.com>
2024-01-08 01:23:49 -0500

    Update Ex03 - Enrollment university - create csv file and student data in list

--- Enrollment_university.py
+++ Enrollment_university.py
@@ -1,5 +1,9 @@
+#Import the libraries
+import os
+import csv
+
 #Declaration variables
-available_countries = ["London", "Manchester", "Liverpool"]
+available_cities = ["London", "Manchester", "Liverpool"]
 available_programs = ["Computer Science", "Medicine", "Marketing", "Arts"]
 available_options = ["1", "2", "3", "4"]
 access_login = False
@@ -30,6 +34,22 @@ def login_user():
                 access = False
     return access
 
+#Funtion to verify the existence of .csv file. If it doesn't exist, it's created
+def file_exists():
+    file_csv_exists = os.path.isfile('enrollment.csv')
+    if not file_csv_exists:
+        with open('enrollment.csv', 'w') as file:
+            file.write('')
+
+#Function to place student data in a list
+def student_list(name, last_name, program, city):
+    student_data = [0,0,0,0]
+    student_data[0] = name
+    student_data[1] = last_name
+    student_data[2] = program
+    student_data[3] = city
+    return student_data
+
 #Welcome message
 menu_01 = """
 Welcome to the university enrollment system:
@@ -49,7 +69,7 @@ Password: Cris 4
 
 #Country Options
 menu_02 = """
-Availables Countries
+Availables Cities
 1. London
 2. Manchester
 3. Liverpool"""
@@ -63,22 +83,29 @@ Availables programs:
 4. Arts
 """
 
+file_exists()
 print(menu_01)
 access_login = login_user()
 
 if access_login == True:
+    #SHow the initial menu and request registration information
     print("***********  Welcome again  ***********\n")
     print("You have the following information to be able to register: ")
     print(menu_02)
     print(menu_03)
     name = input("What's your name?: ")
     last_name = input("What's your last name?: ")
-    chosen_country = input("In which country do you want to enroll?(1-3): ")
+    chosen_city = input("In which of the three cities do you want to enroll?(1-3): ")
     chosen_program = input("What program will you choose?(1-4): ")
-    if chosen_country in available_options[0:3] and chosen_program in available_options[0:4]:
-        print("Todo Gucci")
+    #Validate input data
+    if chosen_city in available_options[0:3] and chosen_program in available_options[0:4]:
+        chosen_city = int(chosen_city)
+        chosen_program = int(chosen_program)
+        student_to_enroll = student_list(name, last_name, available_programs[chosen_program - 1], available_cities[chosen_city - 1])
+        print(student_to_enroll)
+        print("Todo_gucci")
     else:
         message_goodbye("\nWarning: Wrong option chosen")   
 else:
-    print("Warning: Maximum number of attempts")
+    print("\nWarning: Maximum number of attempts")
     message_goodbye("Locked system.")
