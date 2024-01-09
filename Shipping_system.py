4e3aca8877b0a9a7edcc95507230a9999a6b305e
Cristhyan Perez <pevicristhyan@gmail.com>
2024-01-08 23:08:56 -0500

    Update Ex04 - principal structure

--- Shipping_system.py
+++ Shipping_system.py
@@ -16,7 +16,7 @@ def login_attempts(number_attempts):
     for i in range(number_attempts):
         username_in = input("What is your username: ")
         password_in = input("What is your password: ")
-        print(f"\nAttempt {i+1} of {number_attempts}")
+        print(f"\nAttempt {i+1} of {number_attempts}\n")
         check_login = verify_log_in(username_in, password_in)   #True or False
         if check_login == True:
             access_allowed = True
@@ -41,12 +41,12 @@ def list_person(role):
 #Define function assign the random number, and using the weight calculate the amount to pay
 def amount_pay():
     number_random = random.randint(1000000, 9999999)
-    weight = input("Enter the weight of the package: ")
+    weight = input("Enter the weight of the package(kg): ")
     weight = float(weight)
     weight = round(weight, 2)
     while weight <= 0:
         print("\nWarning: wrong Weight")
-        weight = input("Enter the weight of the package: ")
+        weight = input("Enter the weight of the package(Kg): ")
         weight = float(weight)
         weight = round(weight, 2)
     print("\nThe rate is $2 per Kilogram\n")
@@ -65,7 +65,7 @@ def summary_final(sender, recept, n_random, weight_in, amount_out):
     Name: {recept[0]}, Last_name: {recept[1]}, Age: {recept[2]}
     
     Data of package:
-    Weight: {weight_in}
+    Weight: {weight_in} Kg
     rate: $2/Kg
 
     The amount to pay is $ {amount_out}
@@ -84,15 +84,46 @@ Remember that you only have three attempts
 """
 
 #Main menu
-menu_02 ="""
-You have two options:
+menu_02 ="""You have two options:
 1- Send
 2- Exit
-
 """
 
+#Message to exit the program
+def message_custom(sentence):
+    print()
+    print(sentence)
+    print("Thank you")
+    print("Come back soon\n")
 
-
-
-
-
+print(menu_01)
+access_program = login_attempts(3)
+if access_program == True:
+    reset_main_menu = True
+    while reset_main_menu == True:
+        print(menu_02)
+        chosen_option = input("Choose one of the two options (1-2): ")
+        if chosen_option == "1":
+            list_sender = list_person("sender")
+            list_recipient = list_person("recipient")
+            n_rand, weight_pack, amount_final = amount_pay()
+            summary_final(list_sender, list_recipient, n_rand, weight_pack, amount_final)
+            print("\nSuccessful shipment\n")
+            other_operation = input("Do you want to perform another operation (y/n): ")
+            if other_operation == "y" or other_operation == "yes":
+                reset_main_menu = True
+                print("\nLet's go back to the main menu\n")
+            elif other_operation == "n" or other_operation == "no":
+                reset_main_menu = False
+                message_custom("It has been a pleasure to help you")
+            else:
+                reset_main_menu = False
+                message_custom("Wrong option chosen")
+        elif chosen_option == "2":
+            reset_main_menu = False
+            message_custom("It has been a pleasure to help you")
+        else :
+            reset_main_menu = False
+            message_custom("Wrong option chosen")
+else:
+    message_custom("Maximum number of attempts")
