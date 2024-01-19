32e4514627f50b0be4d6d373823ae64d9d2858d9
Cristhyan Perez <pevicristhyan@gmail.com>
2024-01-19 06:40:44 -0500

    Update Ex03 - Enrollment university - code reestructuring, principal functions

--- Enrollment_university.py
+++ Enrollment_university.py
@@ -7,208 +7,95 @@ import pandas as pd
 available_cities = ["London", "Manchester", "Liverpool"]
 available_programs = ["Computer Science", "Medicine", "Marketing", "Arts"]
 available_options = ["1", "2", "3", "4"]
-slots_cities = [1, 3]
-access_login = False
 
-#Program exit message
-def message_goodbye(sentence):
-    print(sentence)
-    print("Thank you")
-    print("\n*****************************\n")
+#Define a function to check if the username and password are correct
+def verify_log_in(username, password):
+    lenght_username = len(username)
+    password_system = username + " " + str(lenght_username)
+    if password_system == password:
+        return True
+    else:
+        return False
 
-#Function to evalauate the three access attempts
-def login_user():
-    for i in range (0,3):
-        username = input("What is your username?: ")
-        number_char = len(username)
-        password_system = username + " " +  str(number_char)
-        password_input = input("What is your password?: ")
-        print(f"\nAttempt {i + 1} of 3")
-        if password_input == password_system:
-            print("Correct username and password\n")
-            access = True
+#Define function to vr¿erify log in in a defined number of attempts
+def login_attempts(number_attempts):
+    access_allowed = False
+    for i in range(number_attempts):
+        username_in = input("What is your username: ")
+        password_in = input("What is your password: ")
+        print(f"\nAttempt {i+1} of {number_attempts}\n")
+        check_login = verify_log_in(username_in, password_in)   #True or False
+        if check_login == True:
+            access_allowed = True
             break
         else:
-            print("Warning: Incorrect password...")
-            if i < 2:
+            print("\nWarning: Incorrect password")
+            if i < ( number_attempts - 1 ):
                 print("Reset username and pasword\n")
                 print("********** Log in **********\n")
-                access = False
-    return access
+    return access_allowed
+
+#Given a question and a list of answers, the function will evaluate if the
+#answer to the question is within the list
+def check_answer(question, list_answers):
+    start_check = False
+    while start_check == False:
+        option_user = input(question)
+        if option_user in list_answers:
+            start_check = True
+        else:
+            print("Wrong option. Let's go again\n")
+    return option_user
 
 #Funtion to verify the existence of .csv file. If it doesn't exist, it's created
-def file_exists():
-    file_csv_exists = os.path.isfile("enrollment.csv")
+def file_exists(file):
+    file_csv_exists = os.path.isfile(file)
     if not file_csv_exists:
-        with open("enrollment.csv", "w") as file:
+        with open(file, "w") as file:
             file.write('')
 
-#Function to delete the last row of the csv file
-def delete_row_csv(file_csv):
-    with open(file_csv, "r", newline="") as file:
-        reader = csv.reader(file)
-        data = list(reader)
-    data.pop()
-    with open(file_csv, "w", newline="") as file:
-        writer = csv.writer(file)
-        writer.writerows(data)
+#Funtion to transform the csv file into a pandas dataframe
+def transform_dataset(file):
+    dataframe = pd.read_csv(file, names = ["name", "last_name", "program", "city"], sep = "\t")
+    return dataframe
 
-#Function to add values to csv file
-def add_values_csv(path, data_list):
-    with open(path, "a", newline = "") as file_csv:
-        write_csv = csv.writer(file_csv, delimiter = "\t")
-        write_csv.writerow(data_list)
-
-#Function to place student data in a list
-def student_list(name, last_name, program, city):
-    student_data = [0,0,0,0]
-    student_data[0] = name
-    student_data[1] = last_name
-    student_data[2] = program
-    student_data[3] = city
-    return student_data
-
-#Slots available per program in each city
+#Function to call the dataset and filter by program, the number of available
+#slots and the slots occupied by city
 def number_slots_available(dataframe, program):
     df_program_london = dataframe[(dataframe["program"] == program) & (dataframe["city"] == "London")]
     df_program_manchester = dataframe[(dataframe["program"] == program) & (dataframe["city"] == "Manchester")]
     df_program_liverpool = dataframe[(dataframe["program"] == program) & (dataframe["city"] == "Liverpool")]
-    slots_available_london = 1 - len(df_program_london)
-    slots_available_manchester = 3 - len(df_program_manchester)
-    slots_available_liverpool = 1 - len(df_program_liverpool)
-    return slots_available_london, slots_available_manchester, slots_available_liverpool
+    slots_available_cities = []
+    slots_available_london = 1 - len(df_program_london)         #There's only 1 slot available in london
+    slots_available_manchester = 3 - len(df_program_manchester) #There's only 3 slot available in manchester
+    slots_available_liverpool = 1 - len(df_program_liverpool)   #There's only 1 slot available in liverpool
+    slots_available_cities.append(slots_available_london)
+    slots_available_cities.append(slots_available_manchester)
+    slots_available_cities.append(slots_available_liverpool)
+    return slots_available_cities
 
-#Function to check if there are places available for the chosen program
-def check_availability(dataframe, program, city):
-    dataframe_filtered = dataframe[(dataframe["city"] == city) & (dataframe["program"] == program)]
-    print(dataframe_filtered)               #Para verificar que columnas está tomando
-    if city == "London" or city == "Liverpool":
-        number_max_program = slots_cities[0] #1 slot available
-    elif city == "Manchester":
-        number_max_program = slots_cities[1] #3 slot available
-    #Count the number of registered
-    number_students = len(dataframe_filtered)
-    #Check if it exceeds the slots
-    if number_students <= number_max_program:
-        check = True    
-    else:
-        check = False
-    return check 
+#Function to show available slots by city
+def show_slots_city(list_cities, list_slots_available, new_list_options):
+    for i in range(3):
+        if list_slots_available[i] == 0:
+            message = "There aren't slots available"
+        elif list_slots_available[i] == 1:
+            message = "There is a slot available"
+            option = str(i + 1) 
+            new_list_options.append(option)
+        else:
+            message = "There are " + list_slots_available[i] + " slots available"
+            new_list_options.append(option)
+        print(f" {i + 1} |  {list_cities[i]}:  {message}")
+    return new_list_options
 
-#print slots availability message and return available reset
-def print_available_slots(dataframe, program, cities_list):
-    av_london, av_manchester, av_liverpool = number_slots_available(dataframe, program)
-    available_list = [0, 0, 0]
-    available_list[0] = av_london
-    available_list[1] = av_manchester
-    available_list[2] = av_liverpool
-    count = 0
-    if av_london == 0 and av_london == 0 and av_london and 0:
-        print(f"\nThere are no slots available in any city for the {program} program")
-        available_reset = False
-    else :
-        print(f"\nFor the {program} program:")
-        for i in available_list:
-            if i <= 0:
-                continue
-            else:
-                print(f"* {i} slot/s availables in {cities_list[count]}")
-            count = count + 1
-        available_reset = False
-        return available_reset
-        
-#Welcome message
-menu_01 = """
-Welcome to the university enrollment system:
+#Funtion to collect user data
+def user_data(program, city):
+    list_user = []
+    name = input("What is your name?: ")
+    last_name = input("What is your last name?: ")
+    list_user.append(name)
+    list_user.append(last_name)
+    list_user.append(city)
+    return list_user
 
-Notes:
-*The password is made up of the following:
-Username + space + number of characteres in the user word
-
-Example:
-Username: Cris
-Password: Cris 4
-
-*You will only have three attempts.
-
-**************  Log in  **************
-"""
-
-#Country Options
-menu_02 = """
-Availables Cities
-1. London
-2. Manchester
-3. Liverpool"""
-
-#Program Options
-menu_03 = """
-Availables programs:
-1. Computer Science
-2. Medicine
-3. Marketing
-4. Arts
-"""
-
-file_exists()
-path_csv_file = os.path.join(os.path.dirname(__file__), "enrollment.csv")
-print(menu_01)
-access_login = login_user()
-
-if access_login == True:
-    #SHow the initial menu and request registration information
-    print("***********  Welcome again  ***********\n")
-    print("You have the following information to be able to register: ")
-    print(menu_02)
-    print(menu_03)
-    name = input("What's your name?: ")
-    last_name = input("What's your last name?: ")
-    chosen_city = input("In which of the three cities do you want to enroll?(1-3): ")
-    chosen_program = input("What program will you choose?(1-4): ")
-    #Validate input data
-    if chosen_city in available_options[0:3] and chosen_program in available_options[0:4]:
-        chosen_city = int(chosen_city)
-        chosen_program = int(chosen_program)
-        student_to_enroll = student_list(name, last_name, available_programs[chosen_program - 1], available_cities[chosen_city - 1])
-        print(student_to_enroll)
-        add_values_csv(path_csv_file, student_to_enroll)
-        #Transform the csv file into a pandas dataframe to be able to manipulate it better
-        dataframe_enrollment = pd.read_csv("enrollment.csv", names = ["name", "last_name", "program", "city"], sep="\t")
-        #Validate if there are places available in the chosen city
-        validate = check_availability(dataframe_enrollment, student_to_enroll[2], student_to_enroll[3])
-        if validate == True:
-            print(f"\nCongratulations {student_to_enroll[0]}\n")
-            print(f"You have enrolled in the program of {student_to_enroll[2]} in the city of {student_to_enroll[3]}")
-        while validate == False:      
-            print(f"\nWe're sorry {student_to_enroll[0]}\n")
-            print(f"There aren't slots availables in {student_to_enroll[3]} for the {student_to_enroll[2]} program")
-            #To proceed to delete the last row
-            last_row = dataframe_enrollment.index[-1]
-            dataframe_enrollment = dataframe_enrollment.drop(last_row)
-            #To proceed to delete the last row of the csv file
-            delete_row_csv("enrollment.csv")
-            #Show were there are places available for that program
-            reset_available = print_available_slots(dataframe_enrollment, student_to_enroll[2], available_cities)
-            print()
-            chosen_city = input("In which of the three cities do you want to enroll?(1-3): ")
-            if chosen_city in available_options[0:3]:
-                chosen_city = int(chosen_city)
-                student_to_enroll = student_list(name, last_name, available_programs[chosen_program - 1], available_cities[chosen_city - 1])
-                validate = check_availability(dataframe_enrollment, student_to_enroll[2], student_to_enroll[3])
-                if validate == True:
-                    print(f"\nCongratulations {student_to_enroll[0]}\n")
-                    print(f"You have enrolled in the program of {student_to_enroll[2]} in the city of {student_to_enroll[3]}")
-                else:
-                    print("\nWarning: Wrong option chosen")
-                    print("Chosen again\n")
-                    validate = False 
-            else:
-                print("\nWarning: Wrong option chosen")
-                print("Chosen again\n")
-                validate = False 
-        print("Todo_gucci")
-    else:
-        message_goodbye("\nWarning: Wrong option chosen")   
-else:
-    print("\nWarning: Maximum number of attempts")
-    message_goodbye("Locked system.")
